92cbdecc8c77fe2612966ab01b7ab8a5809d339a
owaiskhan3 <owais@sudofy.com>
2021-10-03 03:16:06 +0500

    adding missing components

---
+++ hacktoberfest2021/src/components/Dog.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const DogImg = styled.img`
+  width: 30%;
+`;
+const CardWidth = styled.div`
+  width: 30rem;
+`;
+
+export default class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <CardWidth className="card border-dark mb-3">
+          <div className="row g-0">
+            <DogImg alt="img" src="img/dog.png" />
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Dog</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </CardWidth>
+      </div>
+    );
+  }
+}
